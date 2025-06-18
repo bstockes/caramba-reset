d7b077ddd1fca6d9ed4fd36b70c63933b415eab6
bstockes <brianne.stockes@gmail.com>
2025-06-18 11:36:12 -0700

    Update ask.js

--- pages/ask.js
+++ pages/ask.js
@@ -1,10 +1,8 @@
-import ChatBox from '../components/ChatBox';
-
 export default function Ask() {
   return (
     <main style={{ padding: '2rem' }}>
       <h1>Ask Carly</h1>
-      <ChatBox />
+      <p>Chat UI coming soon…</p>
     </main>
   );
 }
