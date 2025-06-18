ddc9bdb7c3f82621793ef594f4a2588e245403c3
bstockes <brianne.stockes@gmail.com>
2025-06-18 11:33:16 -0700

    Update ask.js

--- pages/ask.js
+++ pages/ask.js
@@ -1,11 +1,9 @@
-
 import ChatBox from '../components/ChatBox';
 
 export default function Ask() {
   return (
-    <main style={{ fontFamily: 'Arial, sans-serif', padding: '2rem' }}>
-      <h1>🧠 Ask Carly</h1>
-      <p>Get help with anything about your vehicle.</p>
+    <main style={{ padding: '2rem' }}>
+      <h1>Ask Carly</h1>
       <ChatBox />
     </main>
   );
