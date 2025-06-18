523ef1eb5f7d49bd6511647a136e39333a0cf0c5
bstockes <brianne.stockes@gmail.com>
2025-06-18 11:40:47 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
-import NavBar from '../components/NavBar';
+import { ThemeProvider } from '../components/ThemeProvider';
+import '../styles/themes.css';
 import '../styles/globals.css';
+import NavBar from '../components/NavBar';
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <ThemeProvider>
       <NavBar />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
